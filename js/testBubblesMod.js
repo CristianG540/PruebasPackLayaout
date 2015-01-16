46dafce7fdbb8c4ca8cf2824529c1b666a1e67cd
cowboy <bebop@iMac-de-David.local>
2015-01-16 09:14:35 -0500

    Se eliminan varios archivos

--- js/testBubblesMod.js
+++ js/testBubblesMod.js
@@ -2,33 +2,31 @@
 /*global d3 */
 var testBubblesMod = (function (window, d3, undefined) {
 
-    var _dataTest = '',
-        _svg,
-        _diameter = 960,
-        _format = d3.format(",d");
+
+    var _private = {
+        dataTest : '',
+        svg      : '',
+        diameter : '',
+        format   : d3.format(',d'),
 
 
-    var _tick = function () {
-        _svg.selectAll(".node")
-            .attr("transform", function (d) {
-                return "translate(" + d.x + "," + d.y + ")";
-            });
+        tick     : function () {
+            var scope = this;
+
+            scope.svg.selectAll('.node')
+                .attr("transform", function (d) {
+                    return "translate(" + d.x + "," + d.y + ")";
+                });
+        }
     };
 
-
-
-    var setDataTest = function (data) {
-        _dataTest = data;
+    var public = {
+        setDataTest : function(data) {
+            _private.dataTest = data;
+        }
     };
 
-    var anotherMethod = function () {
-        // public
-    };
-
-    return {
-        setDataTest: setDataTest,
-        anotherMethod: anotherMethod
-    };
+    return public;
 
 })(window, d3);
 
